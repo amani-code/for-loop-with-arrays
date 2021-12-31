b889150ee7b5422d66d7f17154d3ee0e401f5d49
amani-code <85559277+amani-code@users.noreply.github.com>
2021-12-31 10:59:09 +0300

    Add files via upload

---
+++ main (16).cpp
@@ -0,0 +1,18 @@
+//for loop with array 
+//137220 amani shaka chege
+#include <iostream>
+using namespace std;
+
+int
+main ()
+{
+
+  int num[] = { 1, 2, 3, 4, 5 };
+
+  for (int i = 0; i <= 5; i++)
+    {
+      cout << num[i];
+    }
+
+  return 0;
+}
